60448001d7f5a9cff87f79f3f43bf997f194883a
Rafał Trzop <rafaltrzop@gmail.com>
2018-04-28 19:57:08 +0200

    Remove database test files

--- server/create-user.js
+++
@@ -1,8 +0,0 @@
-const User = require('./models/user.model');
-
-User.sync({force: true}).then(() => {
-  return User.create({
-    firstName: 'John',
-    lastName: 'Hancock'
-  });
-});
--- server/database-query.js
+++
@@ -1,5 +0,0 @@
-const User = require('./models/user.model');
-
-User.findAll().then(users => {
-  console.log(users)
-});
