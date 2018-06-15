ce4df40dc0b18b016dc05fb7dfc99c9dc57fddf9
Rafał Trzop <rafaltrzop@gmail.com>
2018-06-15 21:19:39 +0200

    Disable X-Powered-By response header tag

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ const app = express();
 const routes = require('./routes');
 require('./services/passport.service');
 
+app.disable('x-powered-by');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
