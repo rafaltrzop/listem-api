bd3abe65b384801c7b2b63706bfbdb40a3b13c13
Rafał Trzop <rafaltrzop@gmail.com>
2018-07-11 20:18:15 +0200

    Use Helmet to secure Express app

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 const passport = require('passport');
 
 const app = express();
@@ -10,16 +11,17 @@ require('dotenv').config();
 const routes = require('./routes');
 require('./services/passport.service');
 
-app.disable('x-powered-by');
+app.use(helmet());
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
