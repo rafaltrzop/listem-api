54b0a21420c689f7aca4a645a40b31ea9b82f143
Rafał Trzop <rafaltrzop@gmail.com>
2019-01-11 20:38:13 +0100

    Add missing password filed in database query

--- server/services/passport.service.js
+++ server/services/passport.service.js
@@ -7,53 +7,70 @@ const models = require('../models');
 
 const { User } = models;
 
-passport.use('signup', new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-}, async (email, password, done) => {
-  try {
-    const user = await User.create({ email, password });
-    return done(null, { id: user.id, email: user.email });
-  } catch (error) {
-    done(error);
-  }
-}));
+passport.use(
+  'signup',
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.create({ email, password });
+        return done(null, { id: user.id, email: user.email });
+      } catch (error) {
+        done(error);
+      }
+    },
+  ),
+);
 
-passport.use('login', new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-}, async (email, password, done) => {
-  try {
-    const user = await User.findOne({
-      where: { email },
-      attributes: ['id', 'email'],
-    });
+passport.use(
+  'login',
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({
+          where: { email },
+          attributes: ['id', 'email', 'password'],
+        });
 
-    if (!user) {
-      return done(null, false, { message: 'User not found' }); // TODO: change message
-    }
+        if (!user) {
+          return done(null, false, { message: 'User not found' }); // TODO: change message
+        }
 
-    const validate = await user.isValidPassword(password);
-    if (!validate) {
-      return done(null, false, { message: 'Wrong password' }); // TODO: change message
-    }
+        const validate = await user.isValidPassword(password);
+        if (!validate) {
+          return done(null, false, { message: 'Wrong password' }); // TODO: change message
+        }
 
-    return done(null, user, { message: 'Logged in successfully' }); // TODO: remove unnecessary message?
-  } catch (error) {
-    return done(error);
-  }
-}));
+        return done(null, user, { message: 'Logged in successfully' }); // TODO: remove unnecessary message?
+      } catch (error) {
+        return done(error);
+      }
+    },
+  ),
+);
 
-passport.use(new JwtStrategy({
-  secretOrKey: process.env.JWT_SECRET,
-  jwtFromRequest: ExtractJwt.fromExtractors([
-    ExtractJwt.fromAuthHeaderAsBearerToken(),
-    ExtractJwt.fromUrlQueryParameter('token'),
-  ]),
-}, (token, done) => {
-  try {
-    return done(null, token.user);
-  } catch (error) {
-    done(error);
-  }
-}));
+passport.use(
+  new JwtStrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+        ExtractJwt.fromUrlQueryParameter('token'),
+      ]),
+    },
+    (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    },
+  ),
+);
