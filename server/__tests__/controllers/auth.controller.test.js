4cb1f57beb38d0db12838b39155e35312a16f68f
Rafał Trzop <rafaltrzop@gmail.com>
2019-01-13 22:00:25 +0100

    Test API auth with wrong credentials

--- server/__tests__/controllers/auth.controller.test.js
+++ server/__tests__/controllers/auth.controller.test.js
@@ -28,21 +28,71 @@ describe('POST /api/auth', () => {
           expect(res.body.data.token).toMatch(
             /^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?$/,
           );
+
           done();
         });
     });
   });
 
-  // TODO
-  // describe('with wrong credentials', () => {
-  //   test('should respond with error', () => request(app)
-  //     .post('/api/auth')
-  //     .send({
-  //       email: 'test99@gmail.com',
-  //       password: '123456',
-  //     })
-  //     .expect(401));
-  // });
+  describe('with wrong credentials', () => {
+    test('should respond with wrong password error', async (done) => {
+      const user = {
+        email: email(),
+        password: password(),
+      };
+      await User.create(user);
+
+      request(app)
+        .post('/api/auth')
+        .send({ ...user, password: password() })
+        .expect(401)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          expect(res.body.errors.length).toBe(1);
+          expect(res.body.errors[0]).toEqual({ title: 'Wrong password' });
+
+          done();
+        });
+    });
+
+    test('should respond with user not found error', (done) => {
+      const user = {
+        email: email(),
+        password: password(),
+      };
+
+      request(app)
+        .post('/api/auth')
+        .send(user)
+        .expect(401)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          expect(res.body.errors.length).toBe(1);
+          expect(res.body.errors[0]).toEqual({ title: 'User not found' });
+
+          done();
+        });
+    });
+
+    test('should respond with missing credentials error', (done) => {
+      const user = {};
+
+      request(app)
+        .post('/api/auth')
+        .send(user)
+        .expect(401)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          expect(res.body.errors.length).toBe(1);
+          expect(res.body.errors[0]).toEqual({ title: 'Missing credentials' });
+
+          done();
+        });
+    });
+  });
 
   // TODO: global afterEach clean database?
   afterEach(async () => {
