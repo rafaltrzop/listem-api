d532f6f597452823a960e80c1562dd1180469ca6
Rafał Trzop <rafaltrzop@gmail.com>
2020-01-04 20:41:01 +0100

    Fix lint errors in database migrations files

--- src/migrations/20180506083614-create-user.js
+++ src/migrations/20180506083614-create-user.js
@@ -1,39 +1,35 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
       },
-      email: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING,
-        validate: {
-          notEmpty: true,
-          isEmail: true,
-        },
+    },
+    password: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: true,
       },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        validate: {
-          notEmpty: true,
-        },
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  },
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
 };
