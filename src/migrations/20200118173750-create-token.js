50859e175211918f51db8c2aaa2f0903bbcf18d5
Rafał Trzop <rafaltrzop@gmail.com>
2020-01-19 11:14:12 +0100

    Fix lint errors

--- src/migrations/20200118173750-create-token.js
+++ src/migrations/20200118173750-create-token.js
@@ -1,30 +1,25 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Tokens', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      refreshToken: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Tokens');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Tokens', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+    },
+    refreshToken: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Tokens'),
 };
--- src/models/token.js
+++ src/models/token.js
@@ -1,11 +1,17 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Token = sequelize.define('Token', {
-    userId: DataTypes.INTEGER,
-    refreshToken: DataTypes.STRING
-  }, {});
-  Token.associate = function(models) {
-    // associations can be defined here
-  };
+  const Token = sequelize.define(
+    'Token',
+    {
+      userId: DataTypes.INTEGER,
+      refreshToken: DataTypes.STRING,
+    },
+    {},
+  );
+
+  // TODO
+  // associations can be defined here
+  // Token.associate = (models) => {
+  // };
+
   return Token;
 };
