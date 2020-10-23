090b1756358d8ab817b0dcdf7b37aca914820fde
Rafał Trzop <rafaltrzop@gmail.com>
2020-10-23 19:32:43 +0200

    Add deleteExpiredRefreshTokens job test

--- src/jobs/delete-expired-refresh-tokens.js
+++ src/jobs/delete-expired-refresh-tokens.js
@@ -3,13 +3,22 @@ require('dotenv').config();
 
 const { RefreshToken } = require('../models');
 
-const refreshTokenLifetime = new Date(
-  new Date() - process.env.REFRESH_TOKEN_LIFETIME_DAYS * 24 * 60 * 60 * 1000
-);
-RefreshToken.destroy({
-  where: {
-    createdAt: {
-      [Op.lt]: refreshTokenLifetime,
+async function deleteExpiredRefreshTokens() {
+  const expiryDate = new Date(
+    Date.now() - process.env.REFRESH_TOKEN_LIFETIME_DAYS * 24 * 60 * 60 * 1000
+  );
+
+  await RefreshToken.destroy({
+    where: {
+      createdAt: {
+        [Op.lt]: expiryDate,
+      },
     },
-  },
-});
+  });
+}
+
+deleteExpiredRefreshTokens();
+
+module.exports = {
+  deleteExpiredRefreshTokens,
+};
